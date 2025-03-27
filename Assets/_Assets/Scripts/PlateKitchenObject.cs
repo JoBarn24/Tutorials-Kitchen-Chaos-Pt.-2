c6f3c589cb5df15f97a55c3be0c6bfeeede4c2c1
JoBarn24 <157662244+JoBarn24@users.noreply.github.com>
2025-03-26 19:46:42 -0700

    Added Burger Visual on Plate

--- Assets/_Assets/Scripts/PlateKitchenObject.cs
+++ Assets/_Assets/Scripts/PlateKitchenObject.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public class PlateKitchenObject : KitchenObject
 {
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs
+    {
+        public KitchenObjectSO kitchenObjectSO;
+    }
+    
+    [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
+    
     private List<KitchenObjectSO> kitchenObjectSOList;
 
     private void Awake()
@@ -12,6 +21,10 @@ public class PlateKitchenObject : KitchenObject
     
     public bool TryAddIngredient(KitchenObjectSO kitchenObjectSO)
     {
+        if (!validKitchenObjectSOList.Contains(kitchenObjectSO))
+        {
+            return false;
+        }
         if (kitchenObjectSOList.Contains(kitchenObjectSO))
         {
             return false;
@@ -19,6 +32,9 @@ public class PlateKitchenObject : KitchenObject
         else
         {
             kitchenObjectSOList.Add(kitchenObjectSO);
+            
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs { kitchenObjectSO = kitchenObjectSO });
+            
             return true;
         }
     }
---
+++ Assets/_Assets/Scripts/PlatesIconUI.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class PlateIconUI : MonoBehaviour
+{
+    [SerializeField] private PlateKitchenObject plateKitchenObject;
+    
+    
+}
