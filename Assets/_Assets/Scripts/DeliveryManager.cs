3c2ee89cec749ecee8ced5963371ac8bd208fc96
JoBarn24 <157662244+JoBarn24@users.noreply.github.com>
2025-04-09 15:21:53 -0700

    Added Game Timer

--- Assets/_Assets/Scripts/DeliveryManager.cs
+++ Assets/_Assets/Scripts/DeliveryManager.cs
@@ -20,6 +20,7 @@ public class DeliveryManager : MonoBehaviour
     private float spawnRecipeTimer;
     private float spawnRecipeTimerMax = 4f;
     private int waitingRecipesMax = 4;
+    private int successfulRecipesAmount;
 
     private void Awake()
     {
@@ -78,6 +79,7 @@ public class DeliveryManager : MonoBehaviour
                     if (plateContentMatchesRecipe)
                     {
                         //Player delivered the correct recipe
+                        successfulRecipesAmount++;
                         waitingRecipeSOList.RemoveAt(i);
                         OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
                         OnRecipeSuccess?.Invoke(this, EventArgs.Empty);
@@ -94,5 +96,10 @@ public class DeliveryManager : MonoBehaviour
     public List<RecipeSO> GetWaitingRecipeSOList()
     {
         return waitingRecipeSOList;
+    }
+
+    public int GetSuccessfulRecipesAmount()
+    {
+        return successfulRecipesAmount;
     }
 }
