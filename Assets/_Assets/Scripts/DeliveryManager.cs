71d573c762fc4eeb893ee6c921f9fd547da23658
JoBarn24 <157662244+JoBarn24@users.noreply.github.com>
2025-04-09 12:36:05 -0700

    Added Recipes Waiting UI

--- Assets/_Assets/Scripts/DeliveryManager.cs
+++ Assets/_Assets/Scripts/DeliveryManager.cs
@@ -1,8 +1,13 @@
+using System;
 using UnityEngine;
 using System.Collections.Generic;
+using Random = UnityEngine.Random;
 
 public class DeliveryManager : MonoBehaviour
 {
+
+    public event EventHandler OnRecipeSpawned;
+    public event EventHandler OnRecipeCompleted;
     public static DeliveryManager Instance { get; private set; }
     
     [SerializeField] private RecipeListSO recipeListSO;
@@ -25,13 +30,14 @@ public class DeliveryManager : MonoBehaviour
         if (spawnRecipeTimer <= 0f)
         {
             spawnRecipeTimer = spawnRecipeTimerMax;
-        }
-
-        if (waitingRecipeSOList.Count < waitingRecipesMax)
-        {
-            RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[Random.Range(0, recipeListSO.recipeSOList.Count)];
-            Debug.Log(waitingRecipeSO.recipeName);
-            waitingRecipeSOList.Add(waitingRecipeSO);
+            
+            if (waitingRecipeSOList.Count < waitingRecipesMax)
+            {
+                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[UnityEngine.Random.Range(0, recipeListSO.recipeSOList.Count)];
+                waitingRecipeSOList.Add(waitingRecipeSO);
+            
+                OnRecipeSpawned?.Invoke(this, EventArgs.Empty);
+            }
         }
     }
 
@@ -69,15 +75,19 @@ public class DeliveryManager : MonoBehaviour
                     if (plateContentMatchesRecipe)
                     {
                         //Player delivered the correct recipe
-                        Debug.Log("Player delivered the correct recipe!");
                         waitingRecipeSOList.RemoveAt(i);
+                        OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
                         return;
                     }
                 }
             }
             //No matches found
             //Player did not deliver a correct recipe
-            Debug.Log("Player did not deliver the correct recipe!");
         }
+    }
+
+    public List<RecipeSO> GetWaitingRecipeSOList()
+    {
+        return waitingRecipeSOList;
     }
 }
