bde5e02b616c44ed84bd05e96019ed95ca280abb
JoBarn24 <157662244+JoBarn24@users.noreply.github.com>
2025-04-09 20:07:48 -0700

    Added Key Rebinding

--- Assets/_Assets/Scripts/KitchenGameManager.cs
+++ Assets/_Assets/Scripts/KitchenGameManager.cs
@@ -72,7 +72,6 @@ public class KitchenGameManager : MonoBehaviour
             case State.GameOver:
                 break;
         }
-        Debug.Log(state);
     }
 
     public bool IsGamePlaying()
