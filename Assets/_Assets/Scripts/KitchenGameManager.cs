3b42f79145ca6b7ae65f55f4a302a8df2cd995f3
JoBarn24 <157662244+JoBarn24@users.noreply.github.com>
2025-04-15 19:24:28 -0700

    Updated Timer to 3 Minutes

--- Assets/_Assets/Scripts/KitchenGameManager.cs
+++ Assets/_Assets/Scripts/KitchenGameManager.cs
@@ -20,7 +20,7 @@ public class KitchenGameManager : MonoBehaviour
     private State state;
     private float countdownToStartTimer = 3f;
     private float gamePlayingTimer;
-    private float gamePlayingTimerMax = 30f;
+    private float gamePlayingTimerMax = 180f;
     private bool isGamePaused = false;
 
     private void Start()
